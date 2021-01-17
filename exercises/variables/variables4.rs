7220c5ead00ab0da464234bb7cf307ffc92615bf
luanngominh <ngominhluanbox@gmail.com>
2021-01-17 17:37:58 +0700

    init

---
+++ exercises/variables/variables4.rs
@@ -0,0 +1,9 @@
+// variables4.rs
+// Make me compile! Execute the command `rustlings hint variables4` if you want a hint :)
+
+
+fn main() {
+    let mut x: i32;
+    x = 13;
+    println!("Number {}", x);
+}
